f134563febe9ddeb78ace41b117b7dc0ca64cffc
unknown <LDD1@DS3-LDD1-GEO.dsone.3ds.com>
2014-12-12 08:32:32 -0800

    init

---
+++ Product/Product.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+
+
+namespace Product
+{
+
+    internal class ProductStuff
+    {
+        private string name;
+        private decimal price;
+
+        public ProductStuff(string name, decimal price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+        }
+
+        public decimal Price
+        {
+            get
+            {
+                return this.price;
+            }
+        }
+        
+        public static ArrayList GetSampleProducts()
+        {
+            ArrayList list = new ArrayList();
+            list.Add(new ProductStuff("West Side Story", 9.99m));
+            list.Add(new ProductStuff("Assassins", 4.99m));
+            list.Add(new ProductStuff("Frogs", 13.99m));
+            list.Add(new ProductStuff("Sweeny Todd", 10.99m));
+            return list;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}: {1}", 
+                arg0: this.name, arg1: this.price);
+        }
+
+    }
+}
---
+++ Product/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+
+namespace Product
+{
+    
+
+    internal class Program
+    {
+        
+        internal static void Main(string[] args)
+        {
+            
+        }
+    }
+
+}
