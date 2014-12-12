93fcc392519d9bcfae0607117ca2e14fcd41f861
Luke Duguid <luke.DUGUID@3ds.com>
2014-12-12 09:46:45 -0800

    c# in depth - product c# 4 version.

--- Product/Product.cs
+++ Product/Product.cs
@@ -5,39 +5,54 @@ namespace Product
     internal class ProductStuff
     {
         /// <summary>
+        /// C# 4 version from c# indepth
         /// 
-        /// C# 3 version from c# indepth
+        /// Problems with c# version 3.
         /// 
-        /// Problems with c# version 2.
-        /// 
-        /// Automatically implemented properties and simpler initialization
-        /// Now the properties do not have any code (or visible variables!)
-        /// associated with them, and you are building the hardcoded list
-        /// in a very different way. With no name and price variables to
-        /// access, you are forced to use the properties everywhere in the 
-        /// class, improving consistency. You now have a private parameterless
-        /// constructor for the sake of the new property-based initialization.
-        /// The constructor is called for each item before the properties are
-        /// set.
+        /// Named arguements for clear initialization code (C# 4)
+        /// The benefits of specifying the argument names explicilty are
+        /// relatively minimal in this particular example, but when a method
+        /// or constructor has several parameters, it can make the meaning
+        /// of the code much clearer - particularly if they're of the same
+        /// type, or if you are passing in null for some arguements. You can
+        /// choose when to use this feature, of course, only specifying 
+        /// the names for arguments when it makes the code easier to understand.
         /// 
         /// </summary>
-        public ProductStuff()
+        private readonly string name;
+        private readonly decimal price;
+
+        private ProductStuff(string name, decimal price)
         {
+            this.name = name;
+            this.price = price;
         }
 
-        public string Name { get; private set; }
-
-        public decimal Price { get; private set; }
+        private string Name
+        {
+            get
+            {
+                return this.name;
+            }
+        }
+        
+        private decimal Price
+        {
+            get
+            {
+                return this.price;
+            }
+        }
 
         public static List<ProductStuff> GetSampleProducts()
         {
-            return new List<ProductStuff>
-                   {
-                       new ProductStuff { Name = "West Side Story", Price = 9.99m }, 
-                       new ProductStuff { Name = "Assassins", Price = 4.99m }, 
-                       new ProductStuff { Name = "Frogs", Price = 13.99m }, 
-                       new ProductStuff { Name = "Sweeny Todd", Price = 10.99m }
-                   };
+            return new List<ProductStuff> 
+            { 
+                new ProductStuff(name: "West Side Story", price: 9.99m), 
+                new ProductStuff(name: "Assassins", price: 4.99m), 
+                new ProductStuff(name: "Frogs", price: 13.99m), 
+                new ProductStuff(name: "Sweeny Todd", price: 10.99m) 
+            };
         }
 
         public override string ToString()
